9c434e4ff3f401d2dbac99f31137fae963e554c0
dvallon906 <63501917+dvallon906@users.noreply.github.com>
2024-10-24 13:52:22 +0200

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='okapi',
-    version="0.2.1",  # Bump2version mettra à jour cette valeur
+    version="0.2.2",  # Bump2version mettra à jour cette valeur
     description='Un outil pour synchroniser les données entre Raspberry Pi',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
