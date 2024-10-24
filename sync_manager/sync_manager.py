04547d011af7a10fff37b4cad616482d1c7182e2
dvallon906 <63501917+dvallon906@users.noreply.github.com>
2024-10-24 14:02:06 +0200

    Renommer version.py en info.py

--- sync_manager/sync_manager.py
+++ sync_manager/sync_manager.py
@@ -10,7 +10,7 @@ from datetime_util import check_and_sync_time
 from logging_config import setup_logging
 from sync_task import run_sync_task
 
-from version import __version__ as v
+from info import __version__ as v
 
 
 # Obtenir le logger configuré
